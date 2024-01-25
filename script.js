44ca8446ceb4c68b2fbd10b9e689811b99918408
PLD908 <oluyededivine@gmail.com>
2024-01-25 12:12:33 +0100

    Added some code

--- script.js
+++ script.js
@@ -1,17 +1,26 @@
 const BASE_URL = "https://dummyjson.com";
 
 async function login() {
-    const username = document.getElementById('username');
-    const password = document.getElementById('password');
+    const username = document.getElementById('username').value;
+    const password = document.getElementById('password').value;
     try{
         const response = await fetch(`${BASE_URL}/auth/login`, {
             method: "POST",
-            header: {
-                "content-Type": "application/json"
+            headers: {
+                "Content-Type": "application/json"
             },
             body: JSON.stringify({username, password})
-        })
-    }catch(e) {
-
+        });
+        if(response.ok){
+            const data = await response.json();
+            const token = data.token;
+            localStorage.setItem("token", token);
+            window.location.replace("home.html");
+        }else {
+            alert("Login failed, please check your credentials")
+        }
+    }catch(error) {
+        console.error("Error during login", error);
+        alert("An error occured. Please try again."); 
     }
 }
