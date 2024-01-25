5dff9ec0a358333d2720a699471530028e4dbcda
PLD908 <oluyededivine@gmail.com>
2024-01-25 14:41:05 +0100

    Added some code

--- script.js
+++ script.js
@@ -11,11 +11,16 @@ async function login() {
             },
             body: JSON.stringify({username, password})
         });
+        localStorage.setItem("inputPassword", password);
+
         if(response.ok){
             const data = await response.json();
             const token = data.token;
+            const inputUsername = data.username;
             localStorage.setItem("token", token);
             window.location.replace("home.html");
+            localStorage.setItem("inputUsername", inputUsername);
+            console.log(inputUsername);
         }else {
             alert("Login failed, please check your credentials")
         }
@@ -23,4 +28,19 @@ async function login() {
         console.error("Error during login", error);
         alert("An error occured. Please try again."); 
     }
-}
+};
+
+const inputUsername = document.getElementById('username');
+const inputPassword = document.getElementById('password');
+
+inputUsername.addEventListener('click', () => {
+    const username = localStorage.getItem('inputUsername');
+    inputUsername.value = username;
+});
+
+inputPassword.addEventListener('click', () => {
+    const password = localStorage.getItem('inputPassword');
+    const cleanedPassword = password.replace(/["\\]/g, '')
+    inputPassword.value = cleanedPassword;
+    console.log(inputPassword.value);
+});
